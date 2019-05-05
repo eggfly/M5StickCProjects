f046d23d190f45acdc4fd49e54dcc4c579f94cfa
eggfly <eggfly@qq.com>
2019-05-05 22:53:57 +0800

    some audio projects
    Signed-off-by: eggfly <eggfly@qq.com>

---
+++ AMTransmitterI2S/src/Adafruit_GFX/config.h
@@ -0,0 +1,10 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define LCD_WIDTH         80
+#define LCD_HEIGHT        160
+#define BYTES_PER_PIXEL   3
+
+#define GRAM_BUFFER_SIZE (LCD_WIDTH*LCD_HEIGHT*BYTES_PER_PIXEL)
+
+#endif // _CONFIG_H
